369d8b9609a8553da1c9f5b8e4959fbfdd0dea42
David V. Smith <david.victor.smith@gmail.com>
2017-06-02 09:15:43 -0400

    fixed scaling on design

--- figs/plot_model.m
+++ figs/plot_model.m
@@ -1,11 +1,11 @@
 function plot_model()
 % ran this first: grep -v [A-Za-df-z] design.mat | grep [0-9] > design.mtx
-clear;
-close all;
 
 load design.mtx
-%normed_design = mapminmax(design);
-normed_design = zscore(design);
+normed_design = design;
+for i = 1:size(normed_design,2)
+    normed_design(:,i) = rescale(normed_design(:,i),1,2);
+end
 
 
 %normed_design = design;
@@ -13,11 +13,18 @@ normed_design = zscore(design);
 %y = 2*(x - min(x(:))/(max(x(:)) - min(x(:))) - 1);
 %normed_design = y;
 for i = 1:size(normed_design,2)
-    normed_design(:,i) = normed_design(:,i) + i*5;
+    normed_design(:,i) = normed_design(:,i) + i*1.5; %sets the spacing without changing variance/range
     %plot(normed_design(:,i))
 end
 figure,plot(normed_design)
 
-keyboard
 
+end
+
+
+function C = rescale(A,new_min,new_max)
+% rescales entire matrix. 
+current_max = max(A(:));
+current_min = min(A(:));
+C =((A-current_min)*(new_max-new_min))/(current_max-current_min) + new_min;
 end
