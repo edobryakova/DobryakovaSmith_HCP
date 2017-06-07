f16acbf31192a24ee89745c167bfc018de21969d
edobryakova <edobryakova@kesslerfoundation.org>
2017-06-07 12:44:43 -0400

    Parse script change
    changed to extract networks for subs with ER design

--- otherScripts/Parse_dr.m
+++ otherScripts/Parse_dr.m
@@ -1,4 +1,4 @@
-fid = fopen('/Volumes/KATYA5GBA/DRlist_100_blocked.txt','r');
+fid = fopen('/Volumes/KATYA5GBA/DRlist_event.txt','r');
 C = textscan(fid,'%s');
 fclose(fid);
 file_list = C{1};
@@ -6,7 +6,7 @@ file_list = C{1};
 Nfiles = length(file_list);
 for f = 1:Nfiles
     outdir = file_list{f}(1:end-26);
-    thenets = load(fullfile('/Volumes/KATYA5GBA','DRlist_100_blocked',sprintf('dr_stage1_subject%05d.txt',f-1)));
+    thenets = load(fullfile('/Volumes/KATYA5GBA','DRlist_event',sprintf('dr_stage1_subject%05d.txt',f-1)));
     for n = 1:10
         outfile = fullfile(outdir,sprintf('net%02d.txt',n));
         dlmwrite(outfile,thenets(:,n));
